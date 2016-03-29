b6fc2810b0cb965fe8de8c68b8585bbf1ed375c7
ReynotRay <reygarcia718@gmail.com>
2016-03-29 12:34:47 -0500

    list ls clear

--- shopping-list/cart.js
+++ shopping-list/cart.js
@@ -1,16 +1,39 @@
 $(document).ready(function() {
-    $('.list').on('click', 'li', function(event) {
+	 	$('.item1').mouseenter(function() {
+        $('.price').hide();
+        $('.image-plus').show();
+   		});
+
+	  	$('.item1').mouseleave(function() {
+        $('.image-plus').hide();
+        $('.price').show();
+       	});
+
+   		$('.list').on('click', 'li', function(event) {
         $(".cart").append($(this));
-    });
+    	});
 
-	$('.cart').on('click','li', function(event) {
-     $(".list").append($(this));
-	});
+		$('.cart').on('click','li', function(event) {
+     	$(".list").append($(this));
+		});
 
-    $(".checkout").click(function(){
+  	  	$('.cart').mouseenter(function() {
+        $('.image-plus').hide();
+        $('.delete').show();
+  		});
+
+  	    $('.cart').mouseleave(function() {
+        $('.delete').hide();
+        $('.price').show();
+		}); 
+  	
+  		$(".checkout").click(function(){
         alert("Awesome choice!");
+  		});
+  	 });
 
-  	});
-});
-	
+ 
+
+ 
+	// 		<li class="items"><div class="box"></div><span>Chicken</span><img class="delete" src='images/delete.png'/></li>
 //would like to create button to remove shopping cart
