c45dba703026b59cda9371fe00fd3836e3113d29
ReynotRay <reygarcia718@gmail.com>
2016-03-28 22:27:40 -0500

    list

--- shopping-list/cart.js
+++ shopping-list/cart.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
     $('.list').on('click', 'li', function(event) {
         $(".cart").append($(this));
     });
+
 	$('.cart').on('click','li', function(event) {
      $(".list").append($(this));
 	});
+
     $(".checkout").click(function(){
         alert("Awesome choice!");
-	});
 
+  	});
 });
 	
 //would like to create button to remove shopping cart
