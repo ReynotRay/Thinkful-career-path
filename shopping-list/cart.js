dca9ac42cae041b7b4c4359752428e3146b9e631
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-03-29 22:22:19 -0500

    notepad

--- shopping-list/cart.js
+++ shopping-list/cart.js
@@ -1,39 +1,40 @@
 $(document).ready(function() {
-	 	   $('.image1').mouseenter(function() {
+    $('.image1').mouseenter(function() {
         $('.image-plus1').show();
-   		});
-	  	$('.image1').mouseleave(function() {
+    });
+    $('.image1').mouseleave(function() {
         $('.image-plus1').hide();
-       	});
-      $('.image2').mouseenter(function() {
+    });
+    $('.image2').mouseenter(function() {
         $('.image-plus2').show();
-      });
-      $('.image2').mouseleave(function() {
+    });
+    $('.image2').mouseleave(function() {
         $('.image-plus2').hide();
-        });
-       $('.image3').mouseenter(function() {
+    });
+    $('.image3').mouseenter(function() {
         $('.image-plus3').show();
-      });
-      $('.image3').mouseleave(function() {
+    });
+    $('.image3').mouseleave(function() {
         $('.image-plus3').hide();
-        });
-       $('.image4').mouseenter(function() {
+    });
+    $('.image4').mouseenter(function() {
         $('.image-plus4').show();
-      });
-      $('.image4').mouseleave(function() {
+    });
+    $('.image4').mouseleave(function() {
         $('.image-plus4').hide();
-        });
-      $('.cart').mouseenter(function() {
+    });
+    $('.cart').mouseenter(function() {
         $('.image-plus1').hide();
-      });
-
-   		$('.list').on('click','li', function(event) {
+    });
+    $('.list').on('click', 'li', function(event) {
         $(".cart").append($(this));
         $('.remove').show();
-    	});
-      });
-		$('.cart').on('click','li', function(event) {
-     $(".list").append($(this));
-      $('.remove').hide();
-		});
-      
+    });
+    $('.cart').on('click', 'li', function(event) {
+        $(".list").append($(this));
+        $('.remove').hide();
+    });
+    $(".checkout").click(function() {
+        alert("Awesome Choice!!!");
+    });
+});
