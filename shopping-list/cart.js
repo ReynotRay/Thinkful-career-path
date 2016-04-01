c9e36500e8cc0357d4db37e8e0c8dea2688b3bcb
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-04-01 18:24:36 -0500

    changes

--- shopping-list/cart.js
+++ shopping-list/cart.js
@@ -1,21 +1,29 @@
 $(document).ready(function() {
-$('.slist-add').click(function() {
-
-
-    if ($('.slist-input').val().length) {
-        $('.slist').append('<li>' + $('.slist-input').val() + '</li>');
-            }
+    $('.slist-add').keyup(function(event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+            $('slist').click();
+        };
+    });
+    $('.slist-add').click(function() {
+        if ($('.slist-input').val().length) {
+            $('.slist').append('<li>' + $('.slist-input').val() +
+                '</li>');
+        }
+    });
+    $('.slist').on('click', 'li', function() {
+        $(this).css('text-decoration', 'line-through');
+    });
+    //remove items from slist
+    $('.slist').on('click', '.delete', function(e) {
+        e.preventDefault();
+        $(this).parent().remove()
+    });
+    //shows delete button
+    $('.slist').on('mouseenter', 'li', function() {
+        $(this).children('.delete').toggleClass('show');
+    });
+    $('.slist').on('mouseleave', 'li', function() {
+        $(this).children('.delete').toggleClass('show');
+    });
 });
-
-
-$('.slist').on('click', 'li', function() {
-    $(this).css('text-decoration', );
-});
-$('#list').on('click', '.delete', function(e){e.preventDefault(); $(this).parent().remove()});
-
-
-
-   $('.delete').on('click', 'li', function() {
-        $(this).remove()
-});
-   });
