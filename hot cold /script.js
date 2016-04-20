de8686ec1464702e8cabe49bff6bfc6f2c665c2f
ReynotRay <reygarcia718@gmail.com>
2016-04-19 22:19:26 -0500

    hotcold

---
+++ hot cold /script.js
@@ -0,0 +1,58 @@
+"use strict";
+var passGuesses =[];
+
+$(document).ready(function(){
+
+	$('.start-button').click(function() {
+		$('.game')
+		.css('display','block');
+		$('.start-button').hide();
+	});
+	$('.guess-button').click(function() {
+		var userGuessedNumber = $('.input-number') .val();
+		guessTheNumber(userGuessedNumber);
+	});
+	$('.reset').click(function() {
+    location.reload();
+});
+});
+	
+function guessTheNumber(guessedNumber) {
+
+	var randomNumber = parseInt(Math.floor((Math.random() * 100) + 1));
+	console.log('random',randomNumber);
+	console.log('guessed', guessedNumber);
+	//if random number and guessed number equal each other
+	if (randomNumber == guessedNumber) {
+		$('.guess-message').text('Winner!!!!');
+
+	}	else {
+		$('.guess-message').text('keep guessing!');
+	}
+		//if number is not an integer
+  	if(guessedNumber % 1 !== 0){
+  			alert('please input a number');
+  			return true;
+  		}
+  		//if number is less than 0 or more than 100
+  	if(guessedNumber < 0 || guessedNumber > 101){
+  			alert('please choose a number between 1 and 100');
+  			return true;
+  		}
+  		//generate feedback
+  	if(randomNumber == guessedNumber){
+		$('#feedback-message').text('winner-feedback');
+	}
+  	else if ((randomNumber - guessedNumber) < 5){
+		$('#feedback-message').text('hot');
+	}
+	else if ((randomNumber - guessedNumber) < 10){
+		$('#feedback-message').text('warm');
+	}
+	else if ((randomNumber - guessedNumber) < 25){
+		$('#feedback-message').text('cool');
+	}
+	else if ((randomNumber - guessedNumber) < 30){
+		$('#feedback-message').text('horrible guess');
+	}
+}
