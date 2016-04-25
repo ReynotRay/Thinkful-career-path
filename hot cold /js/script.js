b973f0e26ac25d424b94e908e7209c2d94fa39b1
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-04-25 15:29:02 -0500

    hotcold

--- hot cold /js/script.js
+++ hot cold /js/script.js
@@ -1,57 +1,49 @@
-"use strict";
-var passGuesses =[];
+(function() {
+    "use strict";
+    var passGuesses = [];
+    var globalRandomNumber;
+    $(document).ready(function() {
+        $('.start-button').click(function() {
+            $('.game').css('display', 'block');
+            $('.start-button').hide();
+        });
+        $('.guess-button').click(function() {
+            var userGuessedNumber = $('.input-number').val();
+            guessTheNumber(userGuessedNumber);
+        });
+        $('.reset').click(function() {
+            location.reload();
+        });
+    });
 
-$(document).ready(function(){
- 	$('.start-button').click(function() {
-		$('.game')
-		.css('display','block');
-		$('.start-button').hide();
-	});
-	$('.guess-button').click(function() {
-		var userGuessedNumber = $('.input-number') .val();
-		guessTheNumber(userGuessedNumber);
-	});
-		$('.reset').click(function() {
-    	location.reload();
-	});
-});
-	
-function guessTheNumber(guessedNumber) {
-
-	var randomNumber = parseInt(Math.floor((Math.random() * 100) + 1));
-	console.log('random',randomNumber);
-	console.log('guessed', guessedNumber);
-	//if random number and guessed number equal each other
-	if (randomNumber == guessedNumber) {
-		$('.guess-message').text('Winner!!!!');
-
-	}	else {
-		$('.guess-message').text('keep guessing!!!');
-	}
-		//if number is not an integer
-  	if(guessedNumber % 1 !== 0){
-  			alert('please input a number');
-  			return true;
-  		}
-  		//if number is less than 0 or more than 100
-  	if(guessedNumber < 0 || guessedNumber > 101){
-  			alert('please choose a number between 1 and 100');
-  			return true;
-  		}
-  		//generate feedback
-  	if(randomNumber == guessedNumber){
-		$('#feedback-message').text('winner!!!');
-	}
-  	else if ((randomNumber - guessedNumber) < 5){
-		$('#feedback-message').text('hot');
-	}
-	else if ((randomNumber - guessedNumber) < 10){
-		$('#feedback-message').text('warm');
-	}
-	else if ((randomNumber - guessedNumber) < 25){
-		$('#feedback-message').text('cool');
-	}
-	else if ((randomNumber - guessedNumber) < 30){
-		$('#feedback-message').text('horrible guess');
-	}
-}
+    function guessTheNumber(guessedNumber) {
+        if (globalRandomNumber === undefined) {
+            globalRandomNumber = parseInt(Math.floor((Math.random() *
+                100) + 1));
+        }
+        console.log('random', globalRandomNumber);
+        console.log('guessed', guessedNumber);
+        //if number is not an integer
+        if (guessedNumber % 1 !== 0) {
+            alert('please input a number');
+            return true;
+        }
+        //if number is less than 0 or more than 100
+        if (guessedNumber < 0 || guessedNumber > 101) {
+            alert('please choose a number between 1 and 100');
+            return true;
+        }
+        //generate feedback
+        if (globalRandomNumber == guessedNumber) {
+            $('#feedback-message').text('winner!!!');
+        } else if (Math.abs(globalRandomNumber - guessedNumber) < 10) {
+            return $('#feedback-message').text('hot');
+        } else if (Math.abs(globalRandomNumber - guessedNumber) < 20 &&
+            Math.abs(globalRandomNumber - guessedNumber) > 9) {
+            return $('#feedback-message').text('warm');
+        } else if (Math.abs(globalRandomNumber - guessedNumber) < 30 &&
+            Math.abs(globalRandomNumber - guessedNumber) > 19) {
+            return $('#feedback-message').text('cold');
+        }
+    }
+}());
