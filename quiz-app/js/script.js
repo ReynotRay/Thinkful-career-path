ae36ce653d2dd53f459a1281ee2750b209029674
Rey Garcia <reynaldogarcia@Reynaldos-iMac.local>
2016-04-27 15:54:42 -0500

    edits

--- quiz-app/js/script.js
+++ quiz-app/js/script.js
@@ -1,9 +1,39 @@
+	// would like intro $('h1').delay(5000).fadeIn(5000);
+
 $(document).ready(function() {
+
 	$('#start').click(function(){
 		$('#intro').hide();
-		$('#top-question').fadeIn('slow');
+		$('#firstQuestion').fadeIn('slow');
 		$('#main-header').fadeIn('slow');
 		$('#main-section').fadeIn('slow');
+		$('.div-submit').fadeIn('slow');
+		$('.progress').fadeIn('slow');
 		
 	})
+	$('.chicago').click(function(){
+		console.log('youre right');
+
+	})		
+
+
+
+	
 });
+	// var points = 0
+	// var failed =[]
+	// var questions = [
+
+	// image:
+	// answers:['Miami', 'Chicago', 'Japan', 'China'],
+	// correctAnswer:'Miami'
+	// ]
+function checkAnswer(question,answer){
+console.log('in checkAnswer' + question + '' + answer);
+ if(question =='q1' && answer =='Chicago') {
+ 	console.log('youre right');
+ }
+
+};
+
+	
