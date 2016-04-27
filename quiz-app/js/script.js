6287730a23a7150c6b327e812dab87cb51e6885e
Rey Garcia <reynaldogarcia@Reynaldos-iMac.attlocal.net>
2016-04-26 21:53:15 -0500

    quiz

---
+++ quiz-app/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#start').click(function(){
+		$('#intro').hide();
+		$('#top-question').fadeIn('slow');
+		$('#main-header').fadeIn('slow');
+		$('#main-section').fadeIn('slow');
+		
+	})
+});
