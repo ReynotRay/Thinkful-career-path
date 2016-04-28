6d35eb872739d387767187c1fe2cc1d88150d2ce
ReynotRay <reygarcia718@gmail.com>
2016-04-28 16:14:37 -0500

    edits

--- quiz-app/js/script.js
+++ quiz-app/js/script.js
@@ -1,38 +1,51 @@
-	// would like intro $('h1').delay(5000).fadeIn(5000);
 
 $(document).ready(function() {
 
 	$('#start').click(function(){
 		$('#intro').hide();
-		$('#firstQuestion').fadeIn('slow');
+		q1Appear();
+	})
+
+	$('.answer').on('click', function(){
+		console.dir(this);
+		$('.answer').removeClass('selectedAnswer');
+		
+		$(this).addClass('selectedAnswer');
+		console.log('you clicked an answer');
+	})
+});
+
+
+//all functions go here
+	function q1Appear(){
+		$('#firstQuestion').show();
 		$('#header-image').fadeIn('slow');
 		$('#chicago').fadeIn('slow');
-		$('#main-section').fadeIn('slow');
-		$('.answers1').fadeIn('slow');
 		$('.div-submit').fadeIn('slow');
 		$('.progress').fadeIn('slow');
-		
-	})
-	$('#chicago').click(function(){
-		$('#chicago').hide();
-		$('#new-york').fadeIn('slow');
-	})		
+		$('#q1').fadeIn('slow');
+	};
 
-});
-	// var points = 0
-	// var failed =[]
-	// var questions = [
 
-	// image:
-	// answers:['Miami', 'Chicago', 'Japan', 'China'],
-	// correctAnswer:'Miami'
-	// ]
+////checkAnswer here
 function checkAnswer(question,answer){
-console.log('in checkAnswer' + question + '' + answer);
- if(question =='q1' && answer =='Chicago') {
- 	alert('youre right');
- }
+	if (questionNumber == 'q1' && questionAnswer == 'chicago'){
+		console.log('you fuckin got it');
+	}
 
-};
+}
 
-	
+//pull attribute from li was cliked on
+	$('#submitButton').on('click',function(){
+		console.log('you click submit nothing special');
+		console.dir($('.selectedAnswer')[0])
+		
+		var questionNumber = $('.selectedAnswer')[0].parentElement.id;
+		var questionAnswer = $('.selectedAnswer')[0].attr("name");
+
+		checkAnswer(questionNumber,questionAnswer);
+
+	})
+
+
+
