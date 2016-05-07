a85cdef67598709014ff9a204239c8078c4781ed
ReynotRay <reygarcia718@gmail.com>
2016-05-07 11:04:53 -0500

    app

---
+++ ajax-introduction/app.js
@@ -0,0 +1,20 @@
+$(function () {
+
+
+    $('.search-button').click(function () {
+        var search = $('.search').val();
+
+        $.getJSON('http://www.omdbapi.com/?s=' + search + '&r=json', function (data) {
+
+            data.Search.forEach(function (movie) {
+                $('.movies').
+                append('<div class="movie">' + '<h1>' + movie.Title + '</h1>' + '<p>Type: ' + movie.Type + '</p>' + '<span>Year: ' + movie.Year + '</span>' + '<img src=' + movie.Poster + '/>' + '</div>');
+            })
+        });
+    });
+
+
+});
+
+
+//Title
--- quiz-app/TODO.md
+++
@@ -1,3 +0,0 @@
-* Replace console.logs with appropriate messages for the user (e.g. h3 of class message)
-* Save all images locally to images/cities folder 
-* etc.
