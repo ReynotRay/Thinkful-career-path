6de3a82f4e0fb55df9c529360bd6f05db93a9080
ReynotRay <reygarcia718@gmail.com>
2016-05-11 08:01:46 -0500

    logo

---
+++ logoSearch/js/script.js
@@ -0,0 +1,12 @@
+var firstField = document.getElementById('program');
+var secField = document.getElementById('education');
+var image = document.getElementById('image');
+var image = document.getElementById('image2');
+
+
+
+   function equals() {
+        if(firstField.value == secField.value) {
+            image.style.display = 'block';
+  }
+}
