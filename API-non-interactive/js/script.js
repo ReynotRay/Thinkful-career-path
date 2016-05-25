7a793011a686c14fea1ea7c9f5b26092470d8357
ReynotRay <reygarcia718@gmail.com>
2016-05-25 00:13:10 -0500

    edits

---
+++ API-non-interactive/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	//declaring variables
+	var name = [];
+	var memer
+	var description = [];
+	});
--- API/js/script.js
+++ API/js/script.js
@@ -1,7 +1,15 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
-	//declaring variables
-	var name = [];
-	var memer
-	var description = [];
-	});
+// 	//declaring variables
+// 	var name = [];
+// 	var memer
+// 	var description = [];
+// 	});
+
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 29.4241, lng: -98.4936},
+          zoom: 6
+        });
+      }
