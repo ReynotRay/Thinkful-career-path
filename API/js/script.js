75e06f998c9f4f3f387cb7882e17a83e76d1d288
ReynotRay <reygarcia718@gmail.com>
2016-05-25 13:07:24 -0500

    edit

--- API/js/script.js
+++ API/js/script.js
@@ -1,11 +1,3 @@
-// $(document).ready(function(){
-
-// 	//declaring variables
-// 	var name = [];
-// 	var memer
-// 	var description = [];
-// 	});
-
       var map;
       function initMap() {
         map = new google.maps.Map(document.getElementById('map'), {
