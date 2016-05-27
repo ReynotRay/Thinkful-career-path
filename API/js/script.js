2a5f2830586f9173f40fb020d6eb95ae9faba929
ReynotRay <reygarcia718@gmail.com>
2016-05-27 09:23:43 -0500

    read

--- API/js/script.js
+++ API/js/script.js
@@ -1,7 +1,39 @@
-      var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 29.4241, lng: -98.4936},
-          zoom: 6
+window.onload = getMyLocation;
+var maps;
+
+function getMyLocation (){
+	if(navigator.getlocation){
+		navigator.geolocation.getCurrentPosition(showPosition, showerror);
+	} else {
+		alert('no location support');
+	}
+}
+
+$(document).ready(function() {
+	$('.reset').click(function() {
+    location.reload();
+	});      
+});  
+
+
+var map;
+function initMap() {
+	map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 29.4241, lng: -98.4936},
+        zoom: 6
         });
-      }
+ }
+
+
+
+//MEETUP API key 183e4b486a5952514761c514b16755c
+
+
+// 	$.getJSON("http://api.meetup.com/2/open_events.json?zip=10001&radius=7&topic=technology&status=upcoming&time=,1w&key=183e4b486a5952514761c514b16755c=?", function (data) { 
+// var htmlString = ""; 
+// $.each(data.results, function (i, item) {
+//  htmlString += '<h3><a href="' + item.event_url + '" target="_blank">' + item.name + '</a></h3>' + 'People attending: ' + item.yes_rsvp_count + item.time + item.group.name + item.maybe_rsvp_count; }); 
+// $('#upcoming').html(htmlString);
+// }); 
+// }); 
+
--- API/read.md
+++
@@ -1,4 +0,0 @@
-when user types in zip-code, the map will zoom into their location and show nearby meetups.
-
-when user clicks on find current location
-map will zoom into their location and show nearby meetups.
